1136886f9889e7c8706c76e559d1efbff71267b6
hewww <karolak.krzysztof@icloud.com>
2019-08-15 18:56:16 +0200

    zadanie

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -3,6 +3,7 @@ package com.kodilla.stream;
 import com.kodilla.stream.beautifier.PoemBeautifier;
 import com.kodilla.stream.beautifier.PoemDecorator;
 import com.kodilla.stream.beautifier.PoemEditor;
+import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.ExpressionExecutor;
 import com.kodilla.stream.reference.FunctionalCalculator;
 
@@ -26,5 +27,8 @@ public class StreamMain {
         poemBeautifier.beautify("cos", PoemEditor::bigger);
         poemBeautifier.beautify("cos", PoemEditor::addLetters);
         poemBeautifier.beautify("cos", PoemEditor::addDollars);
+
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
 }
