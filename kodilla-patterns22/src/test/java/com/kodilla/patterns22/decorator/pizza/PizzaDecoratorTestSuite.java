458198c6c28a3b59faa164155a435de348037b63
hewww <50962854+hewww@users.noreply.github.com>
2019-11-16 17:35:27 +0100

    Decorator

--- kodilla-patterns22/src/test/java/com/kodilla/patterns22/decorator/pizza/PizzaDecoratorTestSuite.java
+++ kodilla-patterns22/src/test/java/com/kodilla/patterns22/decorator/pizza/PizzaDecoratorTestSuite.java
@@ -15,7 +15,7 @@ public class PizzaDecoratorTestSuite {
         pizzaOrder = new SalamiDecorator(pizzaOrder);
         pizzaOrder = new ExtraCheeseDecorator(pizzaOrder);
         //When
-        System.out.println(pizzaOrder.getName());
+        System.out.println("Koszt pizzy to " + pizzaOrder.getCost() +"zł" + " Twoja pizza: " + pizzaOrder.getName());
         BigDecimal price = pizzaOrder.getCost();
         //Then
         Assert.assertEquals(new BigDecimal(23),price);
@@ -28,7 +28,7 @@ public class PizzaDecoratorTestSuite {
         pizzaOrder = new MuschroomsDecorator(pizzaOrder);
         pizzaOrder = new SalamiDecorator(pizzaOrder);
         //When
-        System.out.println(pizzaOrder.getName());
+        System.out.println("Koszt pizzy to " + pizzaOrder.getCost() +"zł" + " Twoja pizza: " + pizzaOrder.getName());
         String  result = pizzaOrder.getName();
         //Then
         Assert.assertEquals("Basic Pizza with Cheese + Mushrooms + Salami",result);
